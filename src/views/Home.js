e3dddd172427373c54cbb8e48121aa49f843fde6
Sara Arvizu <sara.arvizu07@gmail.com>
2023-12-11 13:21:47 -0600

    Desarrollo de vista home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,18 @@
+import allData from '../data/dataset.js'
+import Card from '../components/Card.js';
+import { Header } from '../components/header.js';
+
 export const Home = () => {
-    const p = document.createElement('p');
-    p.innerHTML = 'Bienvenidos al Home!';
-    return p;
+    const container = document.createElement('span')
+    const ul = document.createElement("ul");
+    const h3 = document.createElement('h3')
+    h3.textContent = 'Now the best pilots around the world:'
+  
+    allData.forEach((driver) => { 
+        ul.appendChild(Card(driver));
+    })
+
+    container.append(Header(), h3,  ul);
+
+    return container;
 };
