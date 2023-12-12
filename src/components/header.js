4b0840bafae6769e0ebb386973f24fcd4a337a0d
Rubi <rubiestram@gmail.com>
2023-12-11 19:05:53 -0600

    Agregue componente estadistica y estilos css

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
-export const Header = () => {
+const Header = () => {
     const header = document.createElement("header");
-    header.classList.add("header")
+    header.classList.add("header");
     const h1 = document.createElement("h1");
     h1.classList.add("title");
     h1.innerHTML = ' Los mejores pilotos de Fórmula 1 de la historia';
     header.appendChild(h1);  // Añadir el elemento h1 al header
     return header;
 }
+
+export default Header;
