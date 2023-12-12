aa360ecbdd331ea85104c30bf083f43c55a9604e
Sara Arvizu <sara.arvizu07@gmail.com>
2023-12-12 13:13:21 -0600

    Componente footer y navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ const Header = () => {
     header.classList.add("header");
     const h1 = document.createElement("h1");
     h1.classList.add("title");
-    h1.innerHTML = ' Los mejores pilotos de Fórmula 1 de la historia';
+    h1.innerHTML = 'Los mejores pilotos de Fórmula 1 de la historia';
     header.appendChild(h1);  // Añadir el elemento h1 al header
     return header;
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    const nav = document.createElement("nav"); //Creamos un elemento.
+    nav.classList.add("navbar"); //Creamos una clase.
+    const divNav = document.createElement("div");
+    divNav.classList.add("menu");
+ 
+ 
+}
+
+export default Navbar
