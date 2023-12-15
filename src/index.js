0d396def618c6ef6401839960131886d24df2576
Sara Arvizu <sara.arvizu07@gmail.com>
2023-12-15 17:12:26 -0600

    Cambios para filtro, odenamiento, boton de eliminar y estadística

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
+// Importa las vistas desde sus respectivos archivos
 import { Home } from './views/Home.js';
 import { Panel } from './views/Panel.js';
 import { Pilot } from './views/Pilot.js';
 import { ApiKey } from './views/ApiKey.js';
 import { ErrorView } from './views/ErrorView.js';
 
+// Importa funciones del router
 import { setRootElement, setRoutes, onURLChange } from './router.js';
 
-// Define your routes and their associated views
+// Define las rutas y sus vistas asociadas
 const routes = {
-  '/': Home,
-  '/panel': Panel,
-  '/pilot': Pilot,
-  '/apikey': ApiKey,
-  '/error': ErrorView,
+  '/': Home,          // Ruta principal
+  '/panel': Panel,    // Ruta para el panel
+  '/pilot': Pilot,    // Ruta para el piloto
+  '/apikey': ApiKey,  // Ruta para la API Key
+  '/error': ErrorView, // Ruta para la vista de error
 };
 
+// Obtiene el elemento raíz del contenedor en el DOM
 const containerRoot = document.querySelector("#root");
 
-// Assign the routes
+// Asigna las rutas
 setRoutes(routes);
+// Establece el elemento raíz donde se renderizarán las vistas
 setRootElement(containerRoot);
 
-// Set the root element where views will be rendered
+// Establece el elemento raíz y maneja el cambio de URL al cargar la página
 window.addEventListener("DOMContentLoaded", (event) => {
     onURLChange(event.currentTarget.location.pathname, {});
     console.log(event.currentTarget);
 });
-
-
