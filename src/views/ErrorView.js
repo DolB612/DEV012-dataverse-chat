dc8a22e1deb82653bf2bf71bcd93cc9f22c6a109
Rubi <rubiestram@gmail.com>
2023-12-18 15:31:49 -0600

    Agregue cambios al CSS y html de la vista error

--- src/views/ErrorView.js
+++ src/views/ErrorView.js
@@ -1,5 +1,15 @@
 export const ErrorView = () => {
-    const p = document.createElement('p');
-    p.innerHTML = "Error 404";
-    return p;
+    const containerError = document.createElement("div");
+    containerError.classList.add("containerError");
+    containerError.innerHTML = `
+    <div class="contentMessage">
+    <h1 class="titleError">Error 404</h1>
+    <h2 class="subtitleError">¡Oh, no!</h2>
+    <p class="textMessage">Nuestros ingenieros están trabajando en la pista digital para resolver cualquier problema técnico.</p>
+    <p class="textMessage">¡Mientras tanto, mantén la calma en el pit y espera a que el semáforo digital te dé luz verde de nuevo!</p>
+    <button class="errorButton">Volver a la carrera</button>
+    </div>
+    `;
+
+    return containerError;
   };
