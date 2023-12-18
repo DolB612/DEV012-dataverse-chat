f3dc538e90069f9779307f89da89bea10bc5e3fc
Sara Arvizu <sara.arvizu07@gmail.com>
2023-12-18 10:39:21 -0600

    Filros, estadística y reset funcionando correctamente

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,17 +1,3 @@
-// const Statistics = () => {
-//     const divStats = document.createElement("div");
-//     divStats.classList.add("computeStats");
-//     const estadistica = document.createElement("p");
-//     estadistica.textContent = 'Promedio de Podios: ';
-//     let spanStats = document.createElement("span");
-//     spanStats.textContent = '--';
-//     spanStats.setAttribute("averagePodiums", "");
-//     estadistica.appendChild(spanStats);
-//     divStats.appendChild(estadistica);
-//     return divStats;
-// }
-
-// export default Statistics;
 // Statistics.js
 const Statistics = () => {
     const divStats = document.createElement("div");
